3dff68edf4b0627aca607c1a2cc35079810b4118
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-21 16:36:55 -0400

    AutH wORKING

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { Switch, Link, Route } from "react-router-dom";
+import { Switch, Link, Route, useHistory } from "react-router-dom";
 import Login from "./components/Login";
 
 const App = () => {
+  const history = useHistory();
   return (
     <div>
       <h1>User DB</h1>
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/login" component={Login} />
+
+        <Route exact path="/home">
           <Link to="/login">Login</Link>
           <br />
           <Link to="/register">Register</Link>
+          <button onClick={() => localStorage.removeItem("token_new")}>
+            Logout
+          </button>
         </Route>
-        <Route exact path="/login" component={Login} />
       </Switch>
     </div>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const history = useHistory();
+
   const [formValues, setForm] = useState({
     username: "",
     password: "",
   });
+
+  const [error, setError] = useState("");
 
   function handleSubmit() {
     axios
       .post("http://localhost:5000/api/auth/login", formValues)
       .then((res) => {
         localStorage.setItem("token_new", res.data.token);
+        history.push("/private");
       })
       .catch((err) => {
-        alert(err);
+        setError(err.response.data.error);
       });
   }
 
@@ -30,6 +36,7 @@ const Login = () => {
       }}
     >
       <h1>Login</h1>
+      {error.length > 0 && <p style={{ color: "red" }}>{error}</p>}
       <input
         type="text"
         placeholder="username"
